3a88991114f19d08f0c7c277a73e9a4d8eefd650
xchange <wyin@xchange.im>
2013-04-21 14:57:35 +0800

    提供54个A站表情，可通过类似[em ac01]的方法在帖子中调用

---
+++ eveclub/community/templatetags/emotion.py
@@ -0,0 +1,14 @@
+#coding: utf-8
+
+from django import template
+
+register = template.Library()
+
+@register.filter(is_safe=True)
+def emotion(value):
+    import re
+    pattern = re.compile(r'\[em (ac)(\d{2})\]')
+    html = r'<img class="emotion_\1" src="/static/img/em/\1\2.gif" />'
+    result, number = pattern.subn(html, value)
+    print result
+    return result
