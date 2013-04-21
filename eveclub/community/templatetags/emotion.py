bb9076dabf3c1f94b17d28c030bd277862701026
xchange <wyin@xchange.im>
2013-04-21 15:33:58 +0800

    修正一处错误 = =|||

--- eveclub/community/templatetags/emotion.py
+++ eveclub/community/templatetags/emotion.py
@@ -4,11 +4,11 @@ from django import template
 
 register = template.Library()
 
+# 支持AC站表情
 @register.filter(is_safe=True)
 def emotion(value):
     import re
     pattern = re.compile(r'\[em (ac)(\d{2})\]')
     html = r'<img class="emotion_\1" src="/static/img/em/\1\2.gif" />'
     result, number = pattern.subn(html, value)
-    print result
     return result
